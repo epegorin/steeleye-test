105128a90fa848806f8c6c05a273f30ea69723e3
epegorin <enzopegorin@gmail.com>
2024-11-17 12:06:28 +0000

    refactor methods to become classes clean temporarry files

--- dataparse/functions.py
+++ dataparse/functions.py
@@ -1,10 +1,34 @@
 import glob
+
 import pandas as pd
 from bs4 import BeautifulSoup
 
+class FinAttrClass:
+    def __init__(self, data, columns):
+        self.df = pd.DataFrame(data=data, columns=columns)
+
+    def store_csv(self, store_path):
+        return self.df.to_csv(store_path, index=False, sep = ';')
+    
+    def count_a(self, source_column, target_column):
+        self.df[target_column] = self.df[source_column].apply(lambda x: str(x).count('a'))
+
+    def contains_a(self, source_column, target_column):
+        self.df[target_column] = self.df[source_column].apply(lambda x: 'YES' if x > 0 else 'NO')
+
+    @classmethod
+    def read_csv(cls, read_path, columns):
+        data = pd.read_csv(read_path, sep=';')
+        return cls(data.to_dict(orient='records'), columns)
 
 def read_local_xml(read_path: str = 'downloads/') -> str:
-    # TODO add docstring
+    """
+    Given an read_path as input, it will read all XML files in the folder and keep the first, then return the xml as a string
+    Args:
+    - read_path (str): folder path to where the XML is stored
+    Return:
+    - xml_file (str): received XML as a string
+    """
 
     xml_files = glob.glob(f'{read_path}*.xml')
 
@@ -15,11 +39,15 @@ def read_local_xml(read_path: str = 'downloads/') -> str:
 
     return xml_file
 
+def parse_xml(xml: str = None, header: list = None) -> list:
+    """
+    Given an an XML file as string, it will find all the required attributes inside FinInstrm > FinInstrmGnlAttrbts > [...]
+    Args:
+    - xml (str): xml parsed as string
+    Return:
+    - xml_file (pd.DataFrame): after selecting only the desired fields from the xml, they are stored as a Pandas Dataframe
+    """
 
-def parse_xml(xml: str = None) -> pd.DataFrame:
-    # TODO add docstring
-
-    header = ['FinInstrmGnlAttrbts.Id', 'FinInstrmGnlAttrbts.FullNm', 'FinInstrmGnlAttrbts.ClssfctnTp', 'FinInstrmGnlAttrbts.CmmdtyDerivInd', 'FinInstrmGnlAttrbts.NtnlCcy', 'Issr']
     table = []
 
     data = BeautifulSoup(xml, features='xml')
@@ -27,38 +55,49 @@ def parse_xml(xml: str = None) -> pd.DataFrame:
     fin_instrm = data.find_all('FinInstrm')
 
     for fin_instrm_item in fin_instrm:
-        Issr = fin_instrm_item.find('Issr').text
         fin_instrm_gnl_attrbts = fin_instrm_item.find('FinInstrmGnlAttrbts')
 
         row = {}
-        for attribute in header[:-1]:
-            row[attribute] = fin_instrm_gnl_attrbts.find(attribute.split('.'[-1])).text
-        row['Issr'] = Issr
+        for attribute in header:
+            if attribute == 'Issr':
+                row[attribute] = fin_instrm_item.find('Issr').text
+            else:
+                row[attribute] = fin_instrm_gnl_attrbts.find(attribute.split('.'[-1])).text
+
 
         table.append(pd.Series(row))
 
-    fin_instrm_df = pd.DataFrame(table, columns = header)
-    return fin_instrm_df
+    # fin_instrm_df = pd.DataFrame(table, columns = header)
+    return table
 
-def df_to_csv(df: pd.DataFrame = None, store_path: str = 'downloads/', delimiter: str = ';') -> None:
-    # TODO add docstring
+# def transform_csv(func, read_path: str = 'downloads/raw_dltins.csv', store_path: str = 'downloads/transformed_dltins.csv', source_column: str = None, target_column: str = None):
 
-    df.to_csv(f'{store_path}raw_dltins.csv', index=False, sep = delimiter)
+#     df = csv_to_df(read_path=read_path, delimiter=';')
+#     df[target_column] = df[source_column].apply(func)
+#     df_to_csv(df=df, store_path=store_path, delimiter=';')
 
-def count_a(full_nm):
-    return 0 if full_nm is None else full_nm.count('a')
-def contains_a(a_count):
-    return 'YES' if a_count > 0 else 'NO'
+# TODO CREATE CLASS OF TYPE DATAFRAME WITH METHODS FOR READ AND WRITE
 
-def transform_csv(func, df: pd.DataFrame, column: str = None):
-
-    df['a_count'] = df[column].apply(func)
-    return df
-
+header = ['FinInstrmGnlAttrbts.Id', 'FinInstrmGnlAttrbts.FullNm', 'FinInstrmGnlAttrbts.ClssfctnTp', 'FinInstrmGnlAttrbts.CmmdtyDerivInd', 'FinInstrmGnlAttrbts.NtnlCcy', 'Issr']
 
 xml_file = read_local_xml(read_path = 'downloads/')
-fin_instrm_df = parse_xml(xml = xml_file)
-# df_to_csv(df = fin_instrm_df)
-transformed_df = transform_csv(func = count_a, df=fin_instrm_df, column = 'FinInstrmGnlAttrbts.FullNm')
-transformed_df = transform_csv(func = contains_a, df=transformed_df, column = 'a_count')
-print(transformed_df)
+fin_instrm_table = parse_xml(xml = xml_file, header=header)
+
+fin_instrm = FinAttrClass(data = fin_instrm_table, columns = header)
+
+fin_instrm.store_csv('downloads/raw_dltins.csv')
+
+fin_instrm.count_a('FinInstrmGnlAttrbts.FullNm', 'a_count')
+fin_instrm.store_csv('downloads/transformed_1_dltins.csv')
+
+fin_instrm.contains_a('a_count', 'contains_a')
+fin_instrm.store_csv('downloads/transformed_2_dltins.csv')
+
+# fin_instrm_df = fin_instrm.df
+# print(fin_instrm_df)
+
+# transform_csv(func = count_a, read_path='downloads/raw_dltins.csv', store_path = 'downloads/transformed_1_dltins.csv', source_column = 'FinInstrmGnlAttrbts.FullNm', target_column = 'a_count')
+# transform_csv(func = contains_a, read_path='downloads/transformed_1_dltins.csv', store_path = 'downloads/transformed_2_dltins.csv', source_column = 'a_count', target_column = 'contains_a')
+
+# fin_instrm_df1 = FinAttrClass.read_csv('downloads/raw_dltins.csv', columns=header)
+# print(fin_instrm_df1.df)
